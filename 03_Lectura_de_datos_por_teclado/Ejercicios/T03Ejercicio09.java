19080dd1efbc1da309a76f6b60bb6c671b0149ef
David Márquez <david.marquez.cordoba@gmail.com>
2023-10-26 20:39:08 +0200

    Añadido T03Ejercicio09

---
+++ 03_Lectura_de_datos_por_teclado/Ejercicios/T03Ejercicio09.java
@@ -0,0 +1,47 @@
+/*
+ * Escribe un programa que calcule el volumen de un cono según la fórmula V = 1/3 pi r2h
+ * 
+ * @author David Márquez Córdoba
+ */
+
+import java.util.*;
+
+
+public class T03Ejercicio09 {
+  public static void main(String[] args) {
+    double radio;
+    double altura;
+    double pi = Math.PI;
+    double radioAlCubo;
+
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.println("");
+    System.out.println("+-------------------------------------------+");
+    System.out.println("|        CALCULAR VOLUMEN DE UN CONO        |");
+    System.out.println("+-------------------------------------------+");
+    System.out.println("");
+    System.out.printf("Escriba el radio de su cono (m): ");
+    
+    radio = teclado.nextDouble();
+    
+    
+    System.out.println("");
+    System.out.printf("Escriba la altura de su cono (m): ");
+
+    altura = teclado.nextDouble();
+    
+
+    teclado.close();
+
+    //LOGICA
+
+    radioAlCubo = Math.pow(radio, 2);
+    double volumen = (pi*radioAlCubo*altura)/3;
+
+    System.out.println("");
+    System.out.printf("Resultado es %.2f m3", volumen);
+    System.out.println("");
+    System.out.println("");
+  }
+}
