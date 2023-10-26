6e11961bc947b8e30db534158fcd7ce213fd0b31
David Márquez <david.marquez.cordoba@gmail.com>
2023-10-26 15:28:59 +0200

    Añadido T03Ejercicio05

---
+++ 03_Lectura_de_datos_por_teclado/Ejercicios/T03Ejercicio05.java
@@ -0,0 +1,38 @@
+/*
+ * Escribe un programa que calcule el área de un rectángulo.
+ * 
+ * @author David Márquez Córdoba
+ */
+
+import java.util.Scanner;
+
+public class T03Ejercicio05 {
+  public static void main(String[] args) {
+    
+    int base;
+    int altura;
+    int area;
+
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.println("+------------------------------------+");
+    System.out.println("|   CALCULA EL ÁREA DEL RECTÁNGULO   |");
+    System.out.println("+------------------------------------+");
+    System.out.println();
+    System.out.printf("Introduce la base del rectángulo: ");
+    base = teclado.nextInt();
+    System.out.printf("Introduce la altura del rectángulo: ");
+    altura = teclado.nextInt();
+
+    System.out.println("+------------------------------------+");
+    System.out.println();
+    teclado.close();
+
+    //Lógica
+    area = base * altura;
+
+    //Salida por pantalla
+    System.out.println("El área de un rectángulo es "+area);
+
+  }
+}
