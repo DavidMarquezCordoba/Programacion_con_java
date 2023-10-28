e0271c38411ab3b61949ab1d26072a14dde81e50
David Márquez <david.marquez.cordoba@gmail.com>
2023-10-28 12:06:33 +0200

    Añadido ejemplo OperadorLogico01

--- 04_Sentencia_condicional_(if_y_switch)/Ejemplos/OperadoresLogicos01.java
+++ 04_Sentencia_condicional_(if_y_switch)/Ejemplos/OperadoresLogicos01.java
@@ -17,13 +17,24 @@ public class OperadoresLogicos01 {
 
     System.out.println("");
     
-    System.out.print("Introduzca un número ");
+    System.out.print("Introduzca un número: ");
     numero = teclado.nextInt();
 
-    if (condition) {
-      
-    } else {
-      
+    if (numero < 1 || numero > 100) {
+      System.out.println("El número debe estar comprendido entre 1 - 100");
+      System.out.print("Tienes otra oportunidad: ");
+      System.out.println("");
+      numero = teclado.nextInt();
     }
+    
+    if (numero == 24) {
+      System.out.println("Acertaste!!");
+      System.out.println("");
+    } else {
+      System.out.println("Lo siento :-( en otra ocasión será");
+      System.out.println("");
+    }
+
+    teclado.close();
   }
 }
