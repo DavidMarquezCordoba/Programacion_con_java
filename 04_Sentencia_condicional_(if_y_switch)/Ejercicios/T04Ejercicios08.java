d1d1c589715b01c951bd5b49dc82395d76ebdef3
David Márquez <david.marquez.cordoba@gmail.com>
2023-10-28 22:31:19 +0200

    Añadida descripción del ejercicio T04Ejercicio08

--- 04_Sentencia_condicional_(if_y_switch)/Ejercicios/T04Ejercicios08.java
+++ 04_Sentencia_condicional_(if_y_switch)/Ejercicios/T04Ejercicios08.java
@@ -1,5 +1,6 @@
 /*
- * Explica tu código aquí
+ * Amplía el programa anterior para que diga la nota del boletín (insuficiente,
+suficiente, bien, notable o sobresaliente).
  * 
  * @author David Márquez Córdoba
  */
