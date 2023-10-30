b775cc985bdcaf7bfaeb047bd079a4bc9564d0b0
David Márquez <david.marquez.cordoba@gmail.com>
2023-10-30 12:54:50 +0100

    Mejora para leer mejor con más facilidad los println

--- 04_Sentencia_condicional_(if_y_switch)/Ejercicios/T04Ejercicio16.java
+++ 04_Sentencia_condicional_(if_y_switch)/Ejercicios/T04Ejercicio16.java
@@ -61,27 +61,34 @@ public class T04Ejercicio16 {
     System.out.println("");
     System.out.println("           Responde con sinceridad, estoy para ayudarte             ");
     System.out.println("");
-    System.out.print("1. Tu pareja parece estar más inquieta de lo normal sin ningún motivo aparente (true/false): ");
+    System.out.print("1. Tu pareja parece estar más inquieta de lo normal sin ningún "+
+                      "motivo aparente (true/false): ");
     respuesta1 = teclado.nextBoolean();
     System.out.print("2. Ha aumentado sus gastos de vestuario (true/false): ");
     respuesta2 = teclado.nextBoolean();
     System.out.print("3. Ha perdido el interés que mostraba anteriormente por ti (true/false): ");
     respuesta3 = teclado.nextBoolean();
-    System.out.print("4. Ahora se afeita y se asea con más frecuencia (si es hombre) o ahora se arregla el pelo y se asea con más frecuencia (si es mujer) (true/false):  ");
+    System.out.print("4. Ahora se afeita y se asea con más frecuencia (si es hombre) o ahora"+
+                    " se arregla el pelo y se asea con más frecuencia (si es mujer) (true/false):  ");
     respuesta4 = teclado.nextBoolean();
     System.out.print("5. No te deja que mires la agenda de su teléfono móvil (true/false):  ");
     respuesta5 = teclado.nextBoolean();
-    System.out.print("6. A veces tiene llamadas que dice no querer contestar cuando estás tú delante (true/false):  ");
+    System.out.print("6. A veces tiene llamadas que dice no querer contestar cuando estás tú "+
+                      "delante (true/false):  ");
     respuesta6 = teclado.nextBoolean();
-    System.out.print("7. Últimamente se preocupa más en cuidar la línea y/o estar bronceado/a (true/false):  ");
+    System.out.print("7. Últimamente se preocupa más en cuidar la línea y/o estar "+
+                      "bronceado/a (true/false):  ");
     respuesta7 = teclado.nextBoolean();
-    System.out.print("8. Muchos días viene tarde después de trabajar porque dice tener mucho más trabajo (true/false):  ");
+    System.out.print("8. Muchos días viene tarde después de trabajar porque dice tener mucho "+
+                      "más trabajo (true/false):  ");
     respuesta8 = teclado.nextBoolean();
     System.out.print("9. Has notado que últimamente se perfuma más (true/false):  ");
     respuesta9 = teclado.nextBoolean();
-    System.out.print("10. Se confunde y te dice que ha estado en sitios donde no ha ido contigo (true/false):  ");
+    System.out.print("10. Se confunde y te dice que ha estado en sitios donde no "+
+                      "ha ido contigo (true/false):  ");
     respuesta10 = teclado.nextBoolean();
 
+    System.out.println("");
 
     teclado.close();
 
