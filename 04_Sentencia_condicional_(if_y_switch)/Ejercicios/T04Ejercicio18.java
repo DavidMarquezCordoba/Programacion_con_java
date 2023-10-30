f67833dc8eeee5c6f3c5ff3fcbf369da71fdc009
David Márquez <david.marquez.cordoba@gmail.com>
2023-10-30 19:12:02 +0100

    añadido T04Ejercicio18

---
+++ 04_Sentencia_condicional_(if_y_switch)/Ejercicios/T04Ejercicio18.java
@@ -0,0 +1,38 @@
+/*
+ * Escribe un programa que diga cuál es la primera cifra de un número entero
+introducido por teclado. Se permiten números de hasta 5 cifras.
+ * 
+ * @author David Márquez Córdoba
+ */
+
+// ESTE NO LO HE CONSEGUIDO
+
+import java.util.Scanner;
+
+public class T04Ejercicio18 {
+  public static void main(String[] args) {
+    
+    Scanner teclado = new Scanner(System.in);
+
+    int iNumero;
+    String sNumero;
+    char valorPrimerDigito;
+
+    System.out.println("");
+    System.out.print("Introduce un número entero: ");
+    iNumero = teclado.nextInt();
+    System.out.println("");
+
+    if (iNumero < 99999) {
+      sNumero = Integer.toString(iNumero);
+      valorPrimerDigito = sNumero.charAt(0);
+      System.out.println("El primer dígito de "+iNumero+" es: "+valorPrimerDigito);
+      System.out.println("");    
+    } else {
+      System.out.println("Solo se permiten números de 5 cifras.");
+      System.out.println("");  
+    }
+
+    teclado.close();
+  }
+}
