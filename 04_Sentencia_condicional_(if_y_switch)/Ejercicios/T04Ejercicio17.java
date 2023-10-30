e228d71beb2072e1d45bb19572b1c51bd3e57834
David Márquez <david.marquez.cordoba@gmail.com>
2023-10-30 19:11:04 +0100

    Arreglos visuales, no hay cambios de código

---
+++ 04_Sentencia_condicional_(if_y_switch)/Ejercicios/T04Ejercicio17.java
@@ -0,0 +1,28 @@
+/*
+ * Escribe un programa que diga cuál es la última cifra de un número entero
+introducido por teclado.
+ * 
+ * @author David Márquez Córdoba
+ */
+
+import java.util.Scanner;
+
+public class T04Ejercicio17 {
+  public static void main(String[] args) {
+    
+    int numero;
+
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.println("");
+    System.out.print("Diga un número entero: ");
+    numero = teclado.nextInt();
+    System.out.println("");
+    
+    teclado.close();
+
+    numero = numero %10;
+    System.out.println("El último número es: "+ numero);
+    System.out.println("");
+  }
+}
