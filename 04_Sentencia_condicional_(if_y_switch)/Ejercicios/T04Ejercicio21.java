41bd4d9cfab8f38c6059b54a23670129c70cb149
David Márquez <david.marquez.cordoba@gmail.com>
2023-11-01 12:06:33 +0100

    Modificación de comentarios

--- 04_Sentencia_condicional_(if_y_switch)/Ejercicios/T04Ejercicio21.java
+++ 04_Sentencia_condicional_(if_y_switch)/Ejercicios/T04Ejercicio21.java
@@ -2,10 +2,14 @@
  * Calcula la nota de un trimestre de la asignatura Programación. El programa
 pedirá las dos notas que ha sacado el alumno en los dos primeros controles.
 Si la media de los dos controles da un número mayor o igual a 5, el alumno
-está aprobado y se mostrará la media. En caso de que la media sea un número
+está aprobado y se mostrará la media. 
+
+En caso de que la media sea un número
 menor que 5, el alumno habrá tenido que hacer el examen de recuperación que
 se califica como apto o no apto, por tanto se debe preguntar al usuario ¿Cuál
-ha sido el resultado de la recuperación? (apto/no apto). Si el resultado
+ha sido el resultado de la recuperación? (apto/no apto). 
+
+Si el resultado
 de la recuperación es apto, la nota será un 5; en caso contrario, se mantiene
 la nota media anterior.
  * 
@@ -47,9 +51,9 @@ public class T04Ejercicio21 {
           System.out.println("");
         } else {
           System.out.println("");
-          System.out.printf("¿Cuál ha sido el resultado de la recuperación?: ");
+          System.out.printf("¿Cuál ha sido el resultado de la recuperación?:");
           ///////////////////////
-          teclado.nextLine(); //¿?¿?¿?
+          teclado.nextLine(); 
           /////////////////////
           aptoNoApto = teclado.nextLine();
           System.out.println("");
