777edd6f7d165709c2a3b9a24c176d3fac49e2ed
David Márquez <david.marquez.cordoba@gmail.com>
2023-11-21 20:52:02 +0100

    Añadido v1 T05Ejercicio20

--- 05_Bucles/Ejercicios/T05Ejercicio20.java
+++ 05_Bucles/Ejercicios/T05Ejercicio20.java
@@ -8,12 +8,11 @@ import java.util.Scanner;
 public class T05Ejercicio20 {
   public static void main(String[] args) {
 
-    Scanner teclado = new Scanner(System.in);
     String caracter;
     int altura;
-
     int nivel= 1; //Esta variable controla que nivel estoy pintando, siendo 1 la cúspide.
-    
+
+    Scanner teclado = new Scanner(System.in);
 
     System.out.printf("Introduce el caracter de relleno: ");
     caracter = teclado.nextLine();
@@ -22,9 +21,45 @@ public class T05Ejercicio20 {
     altura = teclado.nextInt();
 
     int espaciosPorDelante = altura -1;
+    int espaciosPorDentro = 0;
+    int basePiramide = altura * 2;
+
+    //Controlamos que mientras el nivel sea inferior a la altura introducida por teclado, continuamos pintando
+    while (nivel<altura) {
+
+      //Pintamos espacios por delante
+      for (int i = 0; i < espaciosPorDelante; i++) {
+        System.out.print(" ");
+      }
+
+      //Pintamos el caracter correspondiente
+      System.out.print(caracter);
+
+      //Pintamos huecos por dentro
+      for (int i = 0; i < espaciosPorDentro; i++) {
+        System.out.print(" ");
+      }
+
+      //pintamos el segundo caracter (excepto el primer nivel)
+      if (nivel >1) {
+        System.out.print(caracter);
+      }
+
+      System.out.println("");
+      //Aumentamos y reducimos valores para seguir dibujando
+      nivel++;                //Aumentamos niveles o pisos de la pirámide
+      espaciosPorDelante--;   //Reducimos 1 espacio en cada nivel
+      espaciosPorDentro+=2;   //En cada nivel o piso van aumentando 2 espacios en blanco más
+
+    }
+
+    //Pintamos la base
+    for (int i = 0; i < basePiramide; i++) {
+      System.out.print(caracter);
+    }
     
+    // System.out.println(""); //Saltamos al siguiente renglón
 
-
-
+    teclado.close();
   }
 }
