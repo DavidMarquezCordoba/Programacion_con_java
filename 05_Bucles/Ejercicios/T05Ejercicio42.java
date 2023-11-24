ea070a2508281de7b2a8c0cfa7db3270cf22d5eb
David Márquez <david.marquez.cordoba@gmail.com>
2023-11-24 22:33:22 +0100

    Añadido T05Ejercicio42 v1

---
+++ 05_Bucles/Ejercicios/T05Ejercicio42.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/**
+ * Escribe un programa que pida un número entero positivo 
+ * por teclado y que muestre a continuación los 5 números 
+ * consecutivos a partir del número introducido. 
+ * Al lado de cada número se debe indicar si se trata de un primo o no.
+ * 
+ * @author David Márquez Córdoba
+ */
+public class T05Ejercicio42 {
+  public static void main(String[] args) {
+    
+    int numeroIntroducido;
+    // boolean esPrimo=false;
+    String esPrimo = "";
+
+    Scanner teclado = new Scanner(System.in);
+
+    do {
+      System.out.printf("Introduce un número entero positivo: ");
+      numeroIntroducido = teclado.nextInt();
+    } while (numeroIntroducido<0);
+    
+    for (int i = numeroIntroducido+1; i <= numeroIntroducido+5; i++) {
+      System.out.printf("%d ", i);
+
+      esPrimo = " es primo.";
+
+      for (int j = 2; j <= i/2; j++) {
+
+        if (i%j==0) {
+          esPrimo = " no es primo";
+        } 
+        
+      }
+      System.out.printf("%s \n", esPrimo);
+    }
+
+    teclado.close();
+  }
+}
+
+
+//REPASAR
