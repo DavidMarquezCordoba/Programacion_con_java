a08fa0a8d99cf97c23765396409d0b5a0c417fe1
David Márquez <david.marquez.cordoba@gmail.com>
2023-11-29 16:04:05 +0100

    v2 - Cambio total de código por uno que funciona

--- 05_Bucles/Ejercicios/T05Ejercicio53.java
+++ 05_Bucles/Ejercicios/T05Ejercicio53.java
@@ -11,31 +11,25 @@ public class T05Ejercicio53 {
   public static void main(String[] args) {
     
     int numeroIntroducido = 0;
-    int nivel = 0;
-
+    
     Scanner teclado = new Scanner(System.in);
-
 
     System.out.printf("Introduzca la altura de la figura: ");
     numeroIntroducido = teclado.nextInt();
 
     System.out.println("");
 
-    while (nivel <= numeroIntroducido+3) {
-
-      for (int i = 0; i < numeroIntroducido; i++) {
+    //Con el primer for vamos recorriendo niveles, para que en cada iteración el nivel o planta vaya bajando
+    for (int i = 0; i < numeroIntroducido; i++) {
+      //Con el segundo for vamos pintando asteriscos, para que en cada iteración/planta vaya disminuyendo 1
+      for (int j = 0; j < numeroIntroducido-i; j++) {
         System.out.print("*");
       }
-
+      //Añadimos el salto de línea y pinte abajo el siguiente nivel
       System.out.println("");
-
-      nivel++;
-      numeroIntroducido--;
     }
 
-
     System.out.println("");
-
 
     teclado.close();
   }
