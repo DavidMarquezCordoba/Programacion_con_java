80680ba4789bc8a4b9fbe19f92899b10e4ec35b0
David Márquez <david.marquez.cordoba@gmail.com>
2023-12-06 11:57:24 +0100

    v2 - Corregido, ya funciona correctamente, entendido toda la lógica

--- 05_Bucles/Ejercicios/T05Ejercicio67.java
+++ 05_Bucles/Ejercicios/T05Ejercicio67.java
@@ -11,54 +11,75 @@ escalón. La anchura de los escalones siempre es la misma: 4 asteriscos.
 public class T05Ejercicio67 {
   public static void main(String[] args) {
     
-    int escalones = 0;
-    int alturaEscalon = 0;
-    int planta = 1;
-    int alturaTotal = 0;
-    String asterisco = "*";
-    final int ANCHURA = 4;
-    int contadorAltura = 1;
-    int contadorAnchura = 1;
-
     Scanner teclado = new Scanner(System.in);
-
     System.out.print("Introduzca el número de escalones: ");
-    escalones = teclado.nextInt();
-
+    int escalones = teclado.nextInt();
     System.out.print("Introduzca la altura de cada escalón: ");
-    alturaEscalon = teclado.nextInt();
+    int alturaEscalon = teclado.nextInt();
 
-    alturaTotal = escalones * alturaEscalon;
+    for (int i = 1; i <= escalones; i++) {
 
-    // Vamos recorriendo todas las alturas
-    for (int i = 0; i < alturaTotal; i++) {
+      for (int j = 1; j <= alturaEscalon; j++) {
 
-      for (int j = 0; j < contadorAnchura; j++) {
-
-        for (int j2 = 0; j2 < ANCHURA*contadorAltura; j2++) {
-          System.out.print(asterisco);
+        for (int k = 0; k < i; k++) {
+          System.out.print("****");
         }
-        
+        System.out.println();
       }
-      contadorAltura++;
-      contadorAnchura++;
-      System.out.println("");
     }
     teclado.close();
   }
 }
-      // // Vamos iterando para ver cuántas veces se repite la altura del escalon elegido
-      // for (int j1 = 0; j1 < contadorAnchura; j1++) {
 
-      //   //Vamos iterando hasta completar la altura del escalón
-      //   for (int j2 = 0; j2 < alturaEscalon; j2++) {
 
-      //     for (int k = 0; k < ANCHURA; k++) {
-      //       System.out.print(asterisco);
-      //     }
 
-      //     System.out.println("");
-      //   }
 
-      // }
-      // contadorAnchura++;
+// Mi primera versión //
+
+// int escalones = 0;
+// int alturaEscalon = 0;
+// int planta = 1;
+// int alturaTotal = 0;
+// String asterisco = "*";
+// final int ANCHURA = 4;
+// int contadorAltura = 1;
+// int contadorAnchura = 1;
+// int contadorIteraciones = 0;
+// int contadorAux = 1;
+
+// Scanner teclado = new Scanner(System.in);
+
+// System.out.print("Introduzca el número de escalones: ");
+// escalones = teclado.nextInt();
+
+// System.out.print("Introduzca la altura de cada escalón: ");
+// alturaEscalon = teclado.nextInt();
+
+// alturaTotal = escalones * alturaEscalon;
+
+// // Vamos recorriendo todas las alturas
+// for (int i = 0; i < alturaTotal; i++) {
+
+//   // Vamos pintando cada nivel
+//   for (int j = 0; j < contadorAnchura; j++) {
+
+//     for (int j2 = 0; j2 < ANCHURA*contadorAltura; j2++) {
+//       System.out.print(asterisco);
+//     }
+//     contadorIteraciones++;
+    
+//   }
+
+//   //Comprobamos si ha llegado a imprimir la altura del escalón introducido por teclado
+//   if (contadorIteraciones == (alturaEscalon)) {
+//     contadorAnchura++;
+//     contadorAux++;
+//     contadorAltura++;
+//     contadorIteraciones = 0;
+//   }
+  
+//   System.out.println("");
+// }
+
+
+// teclado.close();
