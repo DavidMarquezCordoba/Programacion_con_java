d2c9216d640892e96c9f5d32d130abf97bf30529
David Márquez <david.marquez.cordoba@gmail.com>
2023-12-08 20:16:29 +0100

    Añadido ejemplo de Try Catch

---
+++ 05_Bucles/Ejemplos/T05EjemploTryCatch.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class T05EjemploTryCatch {
+  public static void main(String[] args) {
+    double numero1;
+    double numero2;
+    Scanner sc = new Scanner(System.in);
+
+    try {
+      System.out.println("Introduzca el primer número: ");
+      numero1 = sc.nextInt();
+      System.out.println("Introduzca el primer número: ");
+      numero2 = sc.nextInt();
+      sc.close();
+      System.out.println("La media es = " + (numero1 + numero2) / 2);
+    } catch (NumberFormatException nfe) { // saldria fallo si no introduces un número
+      System.out.println("No se puede introducir caracteres.");
+
+    } catch (Exception e) {
+      System.out.println("Eror de mensaje." + e.getMessage());
+      System.out.println("Clase de excepción." + e.getClass());
+    } finally {
+      System.out.println("Gracias por usar este programa .");
+    }
+  }
+}
