6727bc2d74458046674d1604e89fe25ce383e31e
David Márquez <david.marquez.cordoba@gmail.com>
2023-12-18 13:12:59 +0100

    Añadido T08Ejercicio16 + función en utilidades v1

---
+++ 08_Funciones/Ejercicios/T08Ejercicio16.java
@@ -0,0 +1,14 @@
+import funciones.utilidades;
+
+/**
+ * Muestra los números capicúa que hay entre 1 y 99999.
+ * 
+ * @author David Márquez Córdoba
+ */
+
+public class T08Ejercicio16 {
+  public static void main(String[] args) {
+    
+    utilidades.capicuasEntre1Y99999();
+  }
+}
--- 08_Funciones/Ejercicios/funciones/utilidades.java
+++ 08_Funciones/Ejercicios/funciones/utilidades.java
@@ -298,4 +298,16 @@ dentro de un número y devuelve el trozo correspondiente.
       }
     }
   }
+
+  /**
+   * Función que Muestra los números capicúa que hay entre 1 y 99999.
+  */
+
+  public static void capicuasEntre1Y99999(){
+    for (int i = 1; i < 99999; i++) {
+      if (esCapicua(i)) {
+        System.out.print(i+" "); 
+      }
+    }
+  }
 }
