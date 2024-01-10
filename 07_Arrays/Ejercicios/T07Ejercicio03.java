c1761a66a195710218cc04a6e229fe15b0197e23
David Márquez <david.marquez.cordoba@gmail.com>
2024-01-10 13:56:42 +0100

    Añadido T07Ejercicio03 v1 (mi código no arranca directamente)

---
+++ 07_Arrays/Ejercicios/T07Ejercicio03.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+/**
+ * Escribe un programa que lea 10 números por teclado y que luego los muestre
+en orden inverso, es decir, el primero que se introduce es el último en mostrarse
+y viceversa.
+ * 
+ * @author David Márquez Córdoba
+ */
+
+public class T07Ejercicio03 {
+  public static void main(String[] args) {
+
+    //Mi solución -No se ejecuta si quiera-
+    
+    // Scanner teclado = new Scanner(System.in);
+
+    // int [] num = new int [10];
+
+    // for (int i = 0; i < num.length; i++) {
+    //   num[i] = teclado.nextInt();
+    // }
+
+    // for (int i = num.length -1 ; i >= 0; i--) {
+    //   System.out.println("La posición " + i + " tiene el valor " + num[i]);
+    // }
+
+    // teclado.close();
+
+    //Solución de Luis -Funciona-
+    
+    int[] n = new int[10];
+    int i;
+    
+    System.out.println("Por favor, introduzca 10 números enteros.");
+    System.out.println("Pulse la tecla INTRO después de introducir cada número.");
+
+    for (i = 0; i < 10; i++) {
+      n[i] = Integer.parseInt(System.console().readLine());
+    }
+    
+    System.out.println("\nLos números introducidos, al revés, son los siguientes:");
+    for (i = 9; i >= 0; i--) {
+      System.out.println(n[i]);
+    }
+  }
+}
