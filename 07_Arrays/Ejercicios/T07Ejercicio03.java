263852012bf9df5a3df9b73a7166ea9d417d7aa6
David Márquez <david.marquez.cordoba@gmail.com>
2024-01-11 11:20:10 +0100

    v2 - Corregido, he añadido un mensaje de introducción

--- 07_Arrays/Ejercicios/T07Ejercicio03.java
+++ 07_Arrays/Ejercicios/T07Ejercicio03.java
@@ -10,38 +10,37 @@ y viceversa.
 
 public class T07Ejercicio03 {
   public static void main(String[] args) {
-
-    //Mi solución -No se ejecuta si quiera-
     
-    // Scanner teclado = new Scanner(System.in);
+    Scanner teclado = new Scanner(System.in);
 
-    // int [] num = new int [10];
+    int [] num = new int [10];
 
-    // for (int i = 0; i < num.length; i++) {
-    //   num[i] = teclado.nextInt();
-    // }
+    for (int i = 0; i < num.length; i++) {
+      System.out.print("Introduce un número: ");
+      num [i] = teclado.nextInt();
+    }
 
-    // for (int i = num.length -1 ; i >= 0; i--) {
-    //   System.out.println("La posición " + i + " tiene el valor " + num[i]);
-    // }
+    for (int i = num.length -1 ; i >= 0; i--) {
+      System.out.println("La posición " + i + " tiene el valor " + num[i]);
+    }
 
-    // teclado.close();
+    teclado.close();
 
     //Solución de Luis -Funciona-
     
-    int[] n = new int[10];
-    int i;
+    // int[] n = new int[10];
+    // int i;
     
-    System.out.println("Por favor, introduzca 10 números enteros.");
-    System.out.println("Pulse la tecla INTRO después de introducir cada número.");
+    // System.out.println("Por favor, introduzca 10 números enteros.");
+    // System.out.println("Pulse la tecla INTRO después de introducir cada número.");
 
-    for (i = 0; i < 10; i++) {
-      n[i] = Integer.parseInt(System.console().readLine());
-    }
+    // for (i = 0; i < 10; i++) {
+    //   n[i] = Integer.parseInt(System.console().readLine());
+    // }
     
-    System.out.println("\nLos números introducidos, al revés, son los siguientes:");
-    for (i = 9; i >= 0; i--) {
-      System.out.println(n[i]);
-    }
+    // System.out.println("\nLos números introducidos, al revés, son los siguientes:");
+    // for (i = 9; i >= 0; i--) {
+    //   System.out.println(n[i]);
+    // }
   }
 }
