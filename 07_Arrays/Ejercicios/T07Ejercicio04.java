c91bce0bf19ade7dbb0447aae7d6ca1ae764ab47
David Márquez <david.marquez.cordoba@gmail.com>
2024-01-14 18:06:55 +0100

    v2 - Añadido comentarios para entender mejor y limpieza de código

--- 07_Arrays/Ejercicios/T07Ejercicio04.java
+++ 07_Arrays/Ejercicios/T07Ejercicio04.java
@@ -16,36 +16,30 @@ public class T07Ejercicio04 {
     double [] cuadrado = new double [20];
     double [] cubo = new double [20];
 
-    //Cargamos valores en número[]
+
 
     System.out.println("NÚMERO       CUADRADO        CUBO");
     System.out.println("=================================");
-
+    //Cargamos valores en número[]
     for (int i = 0; i < numero.length; i++) {
       numero [i] = (int) (Math.random()*11);
       System.out.printf("%3d", numero[i]);
-
+      
+      //Cargamos valores en cuadrado[] referenciándonos en el número
       for (int j = 0; j < 1; j++) {
         cuadrado[j] = Math.pow(numero[i], 2);
+        //Reservamos 15 espacios y alineamos a la derecha, casteamos para quedarnos con el número entero
         System.out.printf("%15d", (int)(cuadrado[j]));
 
+        //Cargamos valores en cubo[] referenciándonos en el número 
         for (int k = 0; k < 1; k++) {
           cubo[k] = Math.pow(numero[i], 3);
+          //Reservamos 15 espacios y alineamos a la derecha
           System.out.printf("%15d", (int)cubo[k]);
         }
-        // System.out.println("");
       }
       System.out.println("");
     }
-
     System.out.println("");
-    //Cargamos valores de cuadrado[]
-
-
-
-    // System.out.println("");
-    //Cargamos los valores de cubo
-
-
   }
 }
