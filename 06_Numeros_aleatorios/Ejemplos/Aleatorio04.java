b1b37262150820d6ddad334f703f88a03e5ee981
David Márquez <david.marquez.cordoba@gmail.com>
2024-01-21 23:49:45 +0100

    v2 - Prueba con otros números

--- 06_Numeros_aleatorios/Ejemplos/Aleatorio04.java
+++ 06_Numeros_aleatorios/Ejemplos/Aleatorio04.java
@@ -7,10 +7,11 @@ public class Aleatorio04 {
   public static void main(String[] args) {
 
     System.out.println("");
-    System.out.println("Generar 20 números aleatorios entre 50 y 60\n");
+    System.out.println("Generar 20 números aleatorios entre 50 y 60 [50,61)\n");
 
     for (int i = 1; i <= 20; i++) {
-      System.out.println(i + ". " + ((int) (Math.random() * 11) + 50));
+      System.out.println(i + ". " + ((int) (Math.random() * 11) + 50)); //Restas 61 - 50 = 11 (es lo que pones en la *)
+                                                                        //+ 50 es el número a partir del cual empiezas
     }
   }
 }
