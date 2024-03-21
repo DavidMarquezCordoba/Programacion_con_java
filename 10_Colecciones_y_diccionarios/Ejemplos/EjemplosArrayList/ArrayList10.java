532b7e5344c97dc9c2eff2731a53cc27f5bd00bf
David Márquez <david.marquez.cordoba@gmail.com>
2024-03-21 18:55:56 +0100

    Añadidas mejoras

--- 10_Colecciones_y_diccionarios/Ejemplos/EjemplosArrayList/ArrayList10.java
+++ 10_Colecciones_y_diccionarios/Ejemplos/EjemplosArrayList/ArrayList10.java
@@ -21,10 +21,10 @@ public class ArrayList10 {
     ArrayList<Gato> g = new ArrayList<Gato>();
 
     //Inicializamos cada objeto de tipo Gato a través del métdo .add
-    g.add(new Gato("Garfield", "naranja", "mestizo"));
-    g.add(new Gato("Pepe", "gris", "angora"));
-    g.add(new Gato("Mauri", "blanco", "manx"));
-    g.add(new Gato("Ulises", "marrón", "persa"));    
+    g.add(new Gato("Garfield", "naranja", "mestizo", 1));
+    g.add(new Gato("Pepe", "gris", "angora", 2));
+    g.add(new Gato("Mauri", "blanco", "manx", 3));
+    g.add(new Gato("Ulises", "marrón", "persa", 4));    
 
     for (Gato gatos : g) {
       System.out.println(gatos);
